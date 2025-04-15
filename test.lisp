0691516da13f0b78c6267205bb93388297d806f4
Swapneil Singh <swapneil.singh@gmail.com>
2025-04-14 20:57:21 -0700

    Refactor tests and add test for `let-propagation-spec` edge case

--- test.lisp
+++ test.lisp
@@ -1,7 +1,14 @@
 (uiop:define-package :cl-constraints/test
-  (:mix :cl-constraints :try))
+  (:mix :cl-constraints :try :alexandria :serapeum))
 
 (in-package :cl-constraints/test)
+
+(defmacro no-constrain-errors (form)
+  `(handler-case (and (macroexpand ',form) t)
+     (simple-warning (w)
+       (print "failed")
+       (warn w)
+       nil)))
 
 ;;; FIXME: tests fail even though REPL testing succeeds...
 (deftest basic-constraints ()
@@ -11,11 +18,10 @@
             (constrain :non-consing nil
               (identity (nconc a b))))
           '(1 2 3 4)))
-  (is (handler-case
-          (macroexpand '(constrain :non-consing nil
-                         (identity (nconc '(1 2) '(3 4)))))
-        (simple-warning (w) (print "failed")
-          (warn w) nil)))
+  (is
+   (no-constrain-errors
+    (constrain :non-consing nil
+      (identity (nconc '(1 2) '(3 4))))))
   (is (equal
        (constrain :non-mutating nil
          (let ((a 3))
@@ -25,15 +31,13 @@
              (- 3 2))))
        5))
   (is
-   (handler-case
-       (macroexpand '(constrain :non-mutating nil
-                      (let ((a 3))
-                        (declare (ignore a))
-                        (prog1
-                            (+ 3 2)
-                          (- 3 2)))))
-     (simple-warning (w) (print "failed")
-       (warn w) nil)))
+   (no-constrain-errors
+    (constrain :non-mutating nil
+      (let ((a 3))
+        (declare (ignore a))
+        (prog1
+            (+ 3 2)
+          (- 3 2))))) )
   (is (equal
        (constrain :non-mutating nil
          (let ((a 3))
@@ -43,12 +47,18 @@
              (- 3 2))))
        5))
   (is
-   (handler-case
-       (macroexpand '(constrain :non-mutating nil
-                      (let ((a 3))
-                        (declare (ignore a))
-                        (prog2 20
-                            (+ 3 2)
-                          (- 3 2)))))
-     (simple-warning (w) (print "failed")
-       (warn w) nil))))
+   (no-constrain-errors
+    (constrain :non-mutating nil
+         (let ((a 3))
+           (declare (ignore a))
+           (prog2 20
+               (+ 3 2)
+             (- 3 2))))))
+  (is (equal
+       (let ((a (iota 3))
+             (b (iota 3)))
+         (constrain :non-mutating nil (let ((c (append a b))) c)))
+       '(0 1 2 0 1 2)))
+  (is
+   (no-constrain-errors
+    (constrain :non-mutating nil (let ((c (append a b))) c)))))
